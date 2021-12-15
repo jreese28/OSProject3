53800793e47340d34f2444d3c10c7b6f992f3c6f
Jnya <jnyareese@Jnyas-MacBook-Pro.local>
2021-12-15 00:56:32 -0500

    added files

---
+++ server.c
@@ -0,0 +1,191 @@
+#include "server.h"
+
+int chat_serv_sock_fd; //server socket
+
+/////////////////////////////////////////////
+// USE THESE LOCKS AND COUNTER TO SYNCHRONIZE
+
+int numReaders = 0; // keep count of the number of readers
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  // mutex lock
+pthread_mutex_t rw_lock = PTHREAD_MUTEX_INITIALIZER;  // read/write lock
+
+/////////////////////////////////////////////
+
+
+char const *server_MOTD = "Thanks for connecting to the BisonChat Server.\n\nchat>";
+
+struct node *head = NULL;
+
+void func(int sockfd)
+{
+    char buff[MAXBUFF];
+    int n;
+
+    for (;;) {
+        bzero(buff, MAXBUFF);
+   
+        read(sockfd, buff, sizeof(buff));
+        printf("From client: %s\t To client : ", buff);
+        bzero(buff, MAXBUFF);
+        n = 0;
+
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+   
+
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+   signal(SIGINT, sigintHandler);
+    
+
+   // Open server socket
+   chat_serv_sock_fd = get_server_socket();
+
+   // step 3: get ready to accept connections
+   if(start_server(chat_serv_sock_fd, BACKLOG) == -1) {
+      printf("start server error\n");
+      exit(1);
+   }
+   
+   printf("Server Launched! Listening on PORT: %d\n", PORT);
+    
+   //Main execution loop
+   while(1) {
+      //Accept a connection, start a thread
+      int new_client = accept_client(chat_serv_sock_fd);
+      if(new_client != -1) {
+         pthread_t new_client_thread;
+         pthread_create(&new_client_thread, NULL, client_receive, (void *)&new_client);
+      }
+   }
+
+   close(chat_serv_sock_fd);
+}
+
+
+int get_server_socket(char *hostname, char *port) {
+    int opt = TRUE;   
+    int master_socket;
+    struct sockaddr_in address; 
+    
+    //create a master socket  
+    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)   
+    {   
+        perror("socket failed");   
+        exit(EXIT_FAILURE);   
+    }   
+     
+    //set master socket to allow multiple connections ,  
+    //this is just a good habit, it will work without this  
+    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,  
+          sizeof(opt)) < 0 )   
+    {   
+        perror("setsockopt");   
+        exit(EXIT_FAILURE);   
+    }   
+     
+    //type of socket created  
+    address.sin_family = AF_INET;   
+    address.sin_addr.s_addr = INADDR_ANY;   
+    address.sin_port = htons( PORT );   
+         
+    //bind the socket to localhost port 8888  
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   
+    {   
+        perror("bind failed");   
+        exit(EXIT_FAILURE);   
+    }   
+
+   return master_socket;
+}
+
+
+int start_server(int serv_socket, int backlog) {
+   int status = 0;
+   if ((status = listen(serv_socket, backlog)) == -1) {
+      printf("socket listen error\n");
+   }
+   return status;
+}
+
+
+int accept_client(int serv_sock) {
+   int reply_sock_fd = -1;
+   socklen_t sin_size = sizeof(struct sockaddr_storage);
+   struct sockaddr_storage client_addr;
+   //char client_printable_addr[INET6_ADDRSTRLEN];
+
+   // accept a connection request from a client
+   // the returned file descriptor from accept will be used
+   // to communicate with this client.
+   if ((reply_sock_fd = accept(serv_sock,(struct sockaddr *)&client_addr, &sin_size)) == -1) {
+      printf("socket accept error\n");
+   }
+   return reply_sock_fd;
+}
+
+
+/* Handle SIGINT (CTRL+C) */
+void sigintHandler(int sig_num) {
+   printf("Error:Forced Exit.\n");
+
+int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+   
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+   
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+   
+ //   if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   
+    //if((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) { // bind! 
+    
+     if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
+   
+
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+    len = sizeof(cli);
+   
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server accept the client...\n");
+       func(connfd);
+   
+
+   printf("--------CLOSING ACTIVE USERS--------\n");
+	close(sockfd);
+
+   exit(0);
+}
